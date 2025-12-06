f4e930a108a3b5e60deb1aeefad79b1deb95b18f
gbPagano <guilhermebpagano@gmail.com>
2025-12-06 20:30:06 -0300

    feat: day01 part2

--- src/bin/day01.rs
+++ src/bin/day01.rs
@@ -1,27 +1,52 @@
 advent_of_code_2025::gen_main!(part1, part2);
 
+#[derive(Debug)]
 struct Dial {
     size: i32,
     pub curr: i32,
+    pub zero_stopped: i32,
+    pub zero_passes: i32,
 }
 
 impl Dial {
     fn new(size: i32, curr: i32) -> Self {
-        Self { size, curr }
+        Self {
+            size,
+            curr,
+            zero_stopped: 0,
+            zero_passes: 0,
+        }
     }
 
     fn rotate_left(&mut self, val: i32) {
-        self.curr = (self.curr - val).rem_euclid(self.size + 1);
+        let (div, mod_) = (val / self.size, val % self.size);
+        self.zero_passes += div;
+        if self.curr - mod_ <= 0 && self.curr != 0 {
+            self.zero_passes += 1
+        }
+
+        self.curr = (self.curr - val).rem_euclid(self.size);
+        if self.curr == 0 {
+            self.zero_stopped += 1
+        }
     }
 
     fn rotate_right(&mut self, val: i32) {
-        self.curr = (self.curr + val).rem_euclid(self.size + 1);
+        let (div, mod_) = (val / self.size, val % self.size);
+        self.zero_passes += div;
+        if self.curr + mod_ >= self.size {
+            self.zero_passes += 1
+        }
+
+        self.curr = (self.curr + val).rem_euclid(self.size);
+        if self.curr == 0 {
+            self.zero_stopped += 1
+        }
     }
 }
 
 fn part1(input: String) -> i32 {
-    let mut result = 0;
-    let mut dial = Dial::new(99, 50);
+    let mut dial = Dial::new(100, 50);
     for line in input.lines() {
         match line.chars().nth(0) {
             Some('L') => {
@@ -34,13 +59,24 @@ fn part1(input: String) -> i32 {
             }
             _ => unreachable!(),
         }
-        if dial.curr == 0 {
-            result += 1;
-        }
     }
-    result
+    dial.zero_stopped
 }
 
-fn part2(input: String) {
-    unimplemented!()
+fn part2(input: String) -> i32 {
+    let mut dial = Dial::new(100, 50);
+    for line in input.lines() {
+        match line.chars().nth(0) {
+            Some('L') => {
+                let n: i32 = line[1..].parse().unwrap();
+                dial.rotate_left(n);
+            }
+            Some('R') => {
+                let n: i32 = line[1..].parse().unwrap();
+                dial.rotate_right(n);
+            }
+            _ => unreachable!(),
+        }
+    }
+    dial.zero_passes
 }
