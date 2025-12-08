44babd09257e256a062b8e18174fdbc83824225a
gbPagano <guilhermebpagano@gmail.com>
2025-12-08 12:09:44 -0300

    feat: day02

--- src/bin/day02.rs
+++ src/bin/day02.rs
@@ -1,9 +1,47 @@
 advent_of_code_2025::gen_main!(part1, part2);
 
-fn part1(input: String) {
-    unimplemented!()
+use fancy_regex::Regex;
+
+fn part1(input: String) -> u64 {
+    let re = Regex::new(r"^(\d+)\1$").unwrap();
+    input
+        .trim()
+        .split(',')
+        .map(|r| r.split_once('-').unwrap())
+        .map(|(x_str, y_str)| (x_str.parse::<u64>().unwrap(), y_str.parse::<u64>().unwrap()))
+        .map(|(x, y)| {
+            (x..=y)
+                .into_iter()
+                .map(|num| {
+                    if re.is_match(&num.to_string()).unwrap() {
+                        num
+                    } else {
+                        0
+                    }
+                })
+                .sum::<u64>()
+        })
+        .sum()
 }
 
-fn part2(input: String) {
-    unimplemented!()
+fn part2(input: String) -> u64 {
+    let re = Regex::new(r"^(\d+)(\1)+$").unwrap();
+    input
+        .trim()
+        .split(',')
+        .map(|r| r.split_once('-').unwrap())
+        .map(|(x_str, y_str)| (x_str.parse::<u64>().unwrap(), y_str.parse::<u64>().unwrap()))
+        .map(|(x, y)| {
+            (x..=y)
+                .into_iter()
+                .map(|num| {
+                    if re.is_match(&num.to_string()).unwrap() {
+                        num
+                    } else {
+                        0
+                    }
+                })
+                .sum::<u64>()
+        })
+        .sum()
 }
